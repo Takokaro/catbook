20e1384348107603b1c5f74faa2f5b4588d8c8ac
Takokaro <takokarokiji@gmail.com>
2015-02-02 16:45:53 +0300

    Found some difference from tutorial in tests and do not know what to do

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,10 +27,22 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should be have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: "Cat", last_name: "Cat", email: "catcatcat@gmail.com")
+    user.password = user.password_confirmation = 'qwertyui'
+    
     user.profile_name = "My Profile With Spaces"
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must contain only letters, - or _")
   end
+
+# it doesn't pass
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: "Cat", last_name: "Cat", email: "catcatcat@gmail.com")
+    user.password = user.password_confirmation = 'qwertyui'
+
+    user.profile_name = "Cat_1"
+    assert user.valid? 
+  end
+
 end
